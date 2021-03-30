4d4b30b74ba7fa90fcc1ce11f1e0b2a7be85c331
winderton <winterforkato@gmail.com>
2021-03-30 17:53:39 +0300

    event system

---
+++ EventSystem/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "GLFW/glfw3.h"
+
+#include "window.h"
+
+
+int main()
+{
+	Core::Window& window = Core::Window::getInstanse();
+
+
+	while (!glfwWindowShouldClose(window.getGLFWwindow()))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window.getGLFWwindow());
+	}
+
+
+	return 0;
+}
---
+++ EventSystem/src/window.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include "GLFW/glfw3.h"
+#include <functional>
+#include "event.h"
+namespace Core
+{
+	class Window final
+	{
+	private:
+		std::string name;
+		int width, height;
+		GLFWwindow* window = nullptr;
+		std::function<void(Event&)> fnCallback;
+	public:
+		Window(const Window&) = delete;
+		Window& operator=(const Window&) = delete;
+
+	private:
+		Window(const std::string& name, int width, int height);
+	private:
+		~Window();
+	public:
+		static Window& getInstanse() { static Window instanse("Window", 1280, 720); return instanse; }
+	public:
+		void init(const std::string& name, int width, int height);
+		__forceinline GLFWwindow* getGLFWwindow() const { return window; }
+		void setfnCallback(const std::function<void(Event&)>& fn) { fnCallback = fn; }
+	public:
+		static void mouseMovedCallback(GLFWwindow* window, double x, double y);
+		static void mouseScrollCallback(GLFWwindow* window, double x, double y);
+		static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+		static void keyCallback(GLFWwindow*, int key, int scancode, int action, int mods);
+		static void windowResizeCallback(GLFWwindow* window, int width, int height);
+		static void windowCloseCallback(GLFWwindow* window);
+	};
+}
