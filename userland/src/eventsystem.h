de4be44a3efc1b9fa39332a002c4aed2f65732ea
winderton <winterforkato@gmail.com>
2021-05-09 15:29:36 +0300

    sanbbox update

--- userland/src/eventsystem.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "system.h"
-#include "event.h"
-#include "keyboardevent.h"
--- userland/src/main.cpp
+++ userland/src/main.cpp
@@ -1,10 +1,9 @@
-#include "eventsystem.h"
 #include <serialization.h>
 #include <cassert>
+#include "primitive.h"
 
 
 
-using namespace EventSystem;
 using namespace ObjectModel;
 
 
@@ -41,28 +40,62 @@ int main(int argc, char** argv)
 
 #endif
 
-#if 0
-
-	System Foo("Foo");
-	Event* e = new KeyboardEvent('a', true, false);
-
-	Foo.addEvent(e);
-	KeyboardEvent* kb = static_cast<KeyboardEvent*>(Foo.getEvent());
 
 
-	Foo.serialize();
-
-
-#endif
-
-	int16_t foo = 23;
+	/*int16_t foo = 23;
 	Primitive* p = Primitive::create("int16", Type::I16, foo);
 	Core::Util::retriveNsave(p);
 
 
-	std::vector<int8_t> result = Core::Util::load("int16.abc");
 
-	Primitive pp = Primitive::unpack(result);
+
+	std::vector<int8_t> result = Core::Util::load("int16.abc");
+	int16_t it = 0;
+	Primitive prim = Primitive::unpack(result, it);*/
+
+
+
+
+	int32_t foo = 5;
+	Primitive* p = Primitive::create("int32", Type::I32, foo);
+	//Core::Util::retriveNsave(p);
+
+	std::vector<int32_t> data{ 1,2,3,4 };
+	Array* arr = Array::createArray("array", Type::I32, data);
+	//Core::Util::retriveNsave(arr);
+
+	std::string name = "name";
+	Array* str = Array::createString("string", Type::I8, name);
+	//Core::Util::retriveNsave(str);
+
+
+	Object Test("Foo");
+	Test.addEntity(p);
+	Test.addEntity(arr);
+	Test.addEntity(str);
+	Core::Util::retriveNsave(&Test);
+
+	Object Test2("Bar");
+	Test2.addEntity(p);
+
+	Test.addEntity(&Test2);
+
+	Object win("Bazz");
+	Primitive* winPrim = Primitive::create("int32", Type::I32, foo);
+	win.addEntity(winPrim);
+	Test.addEntity(&win);
+
+	Core::Util::retriveNsave(&Test);
+
+
+	int size = Test.getSize();
+	std::vector<int8_t> objectFromFile = Core::Util::load("Foo.abc");
+
+	int16_t it = 0;
+	Object toPrintObject = Object::unpack(objectFromFile, it);
+	std::cout << "Object: " + toPrintObject.getName() << std::endl;
+	std::cout << "Last object in " << toPrintObject.getName() << ": " << toPrintObject.objects.back().getName() << std::endl;
+
 
 
 	(void)argc;
