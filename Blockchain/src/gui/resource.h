c83b0bca355773ce2017722d0f615ccd8da09c0a
winderton <winterforkato@gmail.com>
2021-12-03 20:17:48 +0300

    win32 gui

---
+++ Blockchain/src/gui/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by bal.rc
+//
+#define IDC_MYICON                      2
+#define IDD_BAL_DIALOG                  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_BAL                         107
+#define IDI_SMALL                       108
+#define IDC_BAL                         109
+#define IDR_MAINFRAME                   128
+#define ID_CONTROL_WATCH                32771
+#define ID_CONTROL_ADD                  32772
+#define ID_BLOCKCHAIN_JOIN              32773
+#define ID_QUIT                         32774
+#define ID_CONTROL_BALANCE              32775
+#define ID_CONTROL_SENDWBT              32776
+#define ID_CONTROL_REQUESTWBT           32777
+#define ID_Menu                         32778
+#define ID_CREATE_QWE                   32779
+#define ID_BLOCKCHAIN_CREATE            32780
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
