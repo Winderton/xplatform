2a6cd73f1b6c15025cdb7937e0040347ff365943
winderton <winterforkato@gmail.com>
2021-12-03 20:13:16 +0300

    Miner Client requests and interface init

--- Blockchain/src/core/Miner.cpp
+++ Blockchain/src/core/Miner.cpp
@@ -106,4 +106,68 @@ namespace Core
 
 
 	
+
+	int Miner::start(HttpServer* server, BlockChain& blockchain, std::vector<int>& peers)
+	{
+		char in;
+		spdlog::info("Test spdlog");
+		spdlog::warn("Easy padding in numbers like {:08d}", 12);
+		spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+		spdlog::error("Some error message with arg: {}", 1);
+		std::cout << "Welcome to WinCoin.\n" << std::endl;
+		std::cout << "If you are creating a new blockchain, type 'y',\n if you are joining the existing, type 'j':\n ";
+		std::cin >> in;
+
+		server->config.port = getAvilablePort();
+		writePort(server->config.port);
+		using json = nlohmann::json;
+		if (in == 'y') blockchain.behave(BlockChain::Stage::GENESIS);
+
+		else if (in == 'j')
+		{
+			peers = readPort("file.txt");
+			blockchain.behave(BlockChain::Stage::JOIN);
+
+			json j;
+			j["port"] = server->config.port;
+			for (int i = 0; i < peers.size() - 1; i++)
+			{
+				int port = peers[i];
+				HttpClient client("localhost:" + std::to_string(port));
+				std::shared_ptr<HttpClient::Response> response = client.request("POST", "/peerpush", j.dump());
+				std::shared_ptr<HttpClient::Response> Defaultresponse = client.request("GET");
+			}
+
+			std::vector<std::string> vect;
+			for (int a = 0; a < peers.size() - 1; a++)
+			{
+				int port = peers[a];
+				HttpClient client("localhost:" + std::to_string(port));
+				std::shared_ptr<HttpClient::Response> response = client.request("GET", "/current");
+				vect.push_back(response->content.string());
+			}
+
+			json chain = json::parse(vect[0]);
+			int max = 0;
+			for (int i = 0; i < vect.size(); i++) {
+				json json_data = json::parse(vect[i]);
+				if (max < json_data["length"].get<int>())
+				{
+					max = json_data["length"].get<int>();
+					chain = json_data;
+				}
+			}
+
+			for (int a = 0; a < chain["length"].get<int>(); a++)
+			{
+				auto block = chain["data"][a];
+				std::vector<std::string> data = block["data"].get<std::vector<std::string>>();
+				blockchain.addBlock(block["difficulty"], block["counter"], block["minedtime"], block["previousHash"], block["hash"], block["nonce"], data);
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
 }
