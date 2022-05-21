6afe89d668a719a73088c72e5dd4efb33810d983
alexey <without email>
2022-05-21 21:10:37 +0300

    fast refactoring

--- java/Client.java
+++ java/Client.java
@@ -6,92 +6,75 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Client {
+    private static final Logger LOG = Logger.getLogger(Client.class.getName());
+    private final int port;
+    private final Scanner scanner;
+    private final String serverAddressRaw;
 
-	private int port;
-	private String ipaddress;
-	private InetAddress serveraddress = null;
-	private DatagramSocket socket;
-	private Thread t2;
-	private String message;
-	private Scanner scanner = new Scanner(System.in);
-	
-	public Client(int port, String ipaddress)  {
-		this.port = port;
-		this.ipaddress = ipaddress;
-	}
-	
-	
-	private void init() {
-		
-		
-		t2 = new Thread(()->receive());
-		
-		try {
-			socket = new DatagramSocket();
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			serveraddress = InetAddress.getByName(ipaddress);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-	
-		t2.start();
-		
-	}
-	
-	
-	public void connect() {
-		init();
-		
-		for (;;)
-		{
-			System.out.print("Enter a message");
-			message = scanner.nextLine();
-			send(message.getBytes());
-		}
-	}
-	
-	
-	private void receive() {
-		byte data[] = new byte[1024];
-		DatagramPacket packet = new DatagramPacket(data, data.length);
-		try {
-			socket.receive(packet);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		process(packet.getData());
-	}
-	
-	
-	private void send(byte data[]) {
-		DatagramPacket packet = new DatagramPacket(data, data.length, serveraddress, port);
-		try {
-			socket.send(packet);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	
-	private void process(byte data[]) {
-		System.out.println("packet from: " + port + ":" + ipaddress);
-		System.out.println("data:" + new String(data));
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		Client client = new Client(8888, "localhost");
-		client.connect();
-	}
-	
+    private InetAddress serverAddress;
+    private DatagramSocket socket;
+
+    public Client(int port, String serverAddressRaw) {
+        this.port = port;
+        this.serverAddressRaw = serverAddressRaw;
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void init() {
+        Thread t2 = new Thread(this::receive);
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            serverAddress = InetAddress.getByName(serverAddressRaw);
+        } catch (UnknownHostException e) {
+            LOG.warning("Can not connect to server: " + serverAddressRaw);
+        }
+
+        t2.start();
+    }
+
+    public void connect() {
+        init();
+        for (; ; ) {
+            System.out.print("Enter a message: ");
+            String message = scanner.nextLine();
+            sendMessage(message);
+        }
+    }
+
+    private void receive() {
+        byte[] data = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+        try {
+            socket.receive(packet);
+        } catch (IOException e) {
+            LOG.warning(String.format("An error while receive a package: %s", e.getMessage()));
+        }
+
+        print(packet.getData());
+    }
+
+    private void sendMessage(String msg) {
+        byte[] msgData = msg.getBytes(StandardCharsets.UTF_8);
+        DatagramPacket packet = new DatagramPacket(msgData, msgData.length, serverAddress, port);
+        try {
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void print(byte[] data) {
+        LOG.info(String.format("Received package from %s:%s", serverAddressRaw, port));
+        LOG.info(String.format("Received data %s", new String(data, StandardCharsets.UTF_8)));
+    }
 }
